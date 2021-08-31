8bb77538a603b2cecef41e16fa59e06bd677fff0
DESKTOP-KS7KKEK\Dongkyun <gkehdrbssla@github.com>
2021-08-31 10:32:02 +0900

    file container add

---
+++ src/main/java/com/example/groupware/container/FileInfo.java
@@ -0,0 +1,12 @@
+package com.example.groupware.container;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class FileInfo {
+    private String basePath = "D:\\downloads\\groupware";	//파일 기본경로
+    private String filePath;		//추가 파일경로
+    private String fileName;		//파일이름
+}
