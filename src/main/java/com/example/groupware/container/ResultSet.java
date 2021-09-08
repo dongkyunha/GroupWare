8cc83fca020845cd8b9fc2506ac6bcbaa5cb3e45
DESKTOP-KS7KKEK\Dongkyun <gkehdrbssla@github.com>
2021-09-08 19:10:14 +0900

    no message

--- src/main/java/com/example/groupware/container/ResultSet.java
+++ src/main/java/com/example/groupware/container/ResultSet.java
@@ -6,7 +6,7 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class ResultSet<T> {
-    private String responseMessage = "success";	//처리 성공여부
+    private String responseMessage = "success";	//성공여부
     private Object resultList;			//결과
 
 
