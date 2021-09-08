9f1a62d615f1d5a2669cd33b66fd791abafeac08
DESKTOP-KS7KKEK\Dongkyun <gkehdrbssla@github.com>
2021-09-08 16:51:43 +0900

    delete response entity 추가

---
+++ src/main/java/com/example/groupware/container/ResultSet.java
@@ -0,0 +1,19 @@
+package com.example.groupware.container;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class ResultSet<T> {
+    private String responseMessage = "success";	//처리 성공여부
+    private Object resultList;			//결과
+
+
+    public ResultSet() {}
+
+    //default
+    public ResultSet(T resultList) {
+        this.resultList = resultList;
+    }
+}
