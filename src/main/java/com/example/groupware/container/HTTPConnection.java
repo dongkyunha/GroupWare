e4beda9612fb82979c51d99bced5fe170a31248d
DESKTOP-KS7KKEK\Dongkyun <gkehdrbssla@github.com>
2021-11-25 23:10:08 +0900

    어렵다 어려워

--- src/main/java/com/example/groupware/container/HTTPConnection.java
+++ src/main/java/com/example/groupware/container/HTTPConnection.java
@@ -8,7 +8,9 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.lang.invoke.MethodType;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -23,26 +25,33 @@ public class HTTPConnection {
         return t;
     }
 
-    public <T> HttpHeaders createHearder(String url, T t, MultiValueMap<?,?> uriVariables){
+    public <T> HttpHeaders createHeaders(String url, String charSet, MultiValueMap<String,String> headersVariables){
         HttpHeaders headers = new HttpHeaders();
+
+        if(charSet == null && headersVariables == null){
+//            headers.setContentType("text/plain");
+//            headers.setAccept("text/plain");
+        }else{
+
+        }
 
         return headers;
     }
 
-    private <T> Map<String, Object> sendHTTP(String url, Class<?> responseType, T t, HttpMethod method, MediaType mediaType, Map<? extends String, ? extends String> uriVariables, MultiValueMap<?,?> multiValueMap ){
-//        if(mediaType != null){
-//            return sendInterface(url, responseType, t, HttpMethod.GET, mediaType, uriVariables, multiValueMap);
-//        }else{
-//            if(t == null){
-//                return sendInterface(url, responseType, null, HttpMethod.POST, mediaType, uriVariables, multiValueMap);
-//            }else{
-//                return sendInterface(url, responseType, t, HttpMethod.POST, mediaType, uriVariables, multiValueMap);
-//            }
-//        }
-        return new HashMap<>();
+    private <T> T sendHTTP(String url, String charSet, Class<? extends T> Clazz, T t, MultiValueMap<? extends String, ? extends String> uriVariables, MediaType mediaType, List<MediaType> acceptMediaType, MultiValueMap<String, String> headersVariables){
+
+        if(mediaType == null){
+            return sendInterface(url, charSet, Clazz, t, HttpMethod.GET, uriVariables, null, null, headersVariables);
+        }else{
+            if(acceptMediaType == null){
+                return sendInterface(url, charSet, Clazz, t, HttpMethod.POST, uriVariables, mediaType, Collections.singletonList(mediaType), headersVariables);
+            }else{
+                return sendInterface(url, charSet, Clazz, t, HttpMethod.POST, uriVariables, mediaType, acceptMediaType, headersVariables);
+            }
+        }
     }
 
-    private <T> Map<String, Object> sendInterface(String url, Class<?> responseType, T t, HttpMethod method, MediaType mediaType, Map<? extends String, ? extends String> uriVariables, MultiValueMap<?,?> multiValueMap){
+    private <T> T sendInterface(String url, String charSet, Class<? extends T> Clazz, T t, HttpMethod method, MultiValueMap<? extends String, ? extends String> uriVariables, MediaType mediaType, List<MediaType> acceptMediaType, MultiValueMap<String, String> headersVariables){
 
         Map<String, Object> result = new HashMap<>();
 
@@ -51,7 +60,7 @@ public class HTTPConnection {
             url = uriBuilder.build().encode().toString();
 
             HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(mediaType);
+            headers = createHeaders(url, charSet, headersVariables);
 
             HttpEntity<?> requestEntity = null;
             if(t != null){
@@ -62,7 +71,7 @@ public class HTTPConnection {
 
             //통신
             RestTemplate restTemplate = new RestTemplate();
-            ResponseEntity<?> response = restTemplate.exchange(url, method, requestEntity, responseType);
+            ResponseEntity<?> response = restTemplate.exchange(url, method, requestEntity, Clazz);
 
             switch (response.getStatusCode()){
                 case OK:
@@ -76,12 +85,13 @@ public class HTTPConnection {
                     log.error("요청 실패");
                     break;
             }
+
+//            T t =  <T> response.getBody();
+
         }catch (Exception e){
             e.getMessage();
-        }finally {
-
         }
 
-        return result;
+        return t;
     }
 }
