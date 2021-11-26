16cf7f7dbe58e6804ddc300127d2b9c3b1c77390
DESKTOP-KS7KKEK\Dongkyun <gkehdrbssla@github.com>
2021-11-27 01:24:21 +0900

    일단 작성해서 올려봄

--- src/main/java/com/example/groupware/container/HTTPConnection.java
+++ src/main/java/com/example/groupware/container/HTTPConnection.java
@@ -1,44 +1,62 @@
 package com.example.groupware.container;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.NestedRuntimeException;
+import org.springframework.expression.AccessException;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.lang.invoke.MethodType;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.rmi.ConnectException;
+import java.util.*;
 
 @Slf4j
 @Component
 public class HTTPConnection {
 
-    public <T> T sendGet(T t){
-        return t;
+    public <T> T sendGet(String url, String charSet, Class<?> Clazz, T t, MultiValueMap<String,String> uriVariables, MediaType mediaType, List<MediaType> acceptMediaType, MultiValueMap<String, String> headersVariables) throws Exception {
+        return (T) sendHTTP(url, charSet, Clazz, t, uriVariables, mediaType, acceptMediaType, headersVariables);
     }
 
-    public <T> T sendPost(T t){
-        return t;
+    public <T> T sendPost(String url, String charSet, Class<?> Clazz, T t, MultiValueMap<String, String> uriVariables, MediaType mediaType, List<MediaType> acceptMediaType, MultiValueMap<String, String> headersVariables) throws Exception {
+
+        return (T) sendHTTP(url, charSet, Clazz, t, uriVariables, mediaType, acceptMediaType, headersVariables);
     }
 
-    public <T> HttpHeaders createHeaders(String url, String charSet, MultiValueMap<String,String> headersVariables){
+    public <T> HttpHeaders createHeaders(MediaType mediaType, List<MediaType> acceptMediaType, MultiValueMap<String,String> headersVariables){
         HttpHeaders headers = new HttpHeaders();
 
-        if(charSet == null && headersVariables == null){
-//            headers.setContentType("text/plain");
-//            headers.setAccept("text/plain");
+        if(mediaType == null && acceptMediaType == null){
+            headers.set("Accept", MediaType.TEXT_PLAIN_VALUE);
+            headers.setContentType(MediaType.TEXT_PLAIN);
         }else{
+            if(CollectionUtils.isEmpty(acceptMediaType)){
+                headers.setAccept(Collections.singletonList(mediaType));
+            }else{
+                headers.setAccept(acceptMediaType);
+            }
+            headers.setContentType(mediaType);
+        }
 
+        if(headersVariables != null){
+            Iterator<String> it = (Iterator<String>) headersVariables.keySet().iterator();
+            while(it.hasNext()){
+                String keyStr = it.next();
+                headers.set(keyStr, headersVariables.getFirst(keyStr));
+            }
         }
 
         return headers;
     }
 
-    private <T> T sendHTTP(String url, String charSet, Class<? extends T> Clazz, T t, MultiValueMap<? extends String, ? extends String> uriVariables, MediaType mediaType, List<MediaType> acceptMediaType, MultiValueMap<String, String> headersVariables){
+    private <T> T sendHTTP(String url, String charSet, Class<? extends T> Clazz, T t, MultiValueMap<? extends String, ? extends String> uriVariables, MediaType mediaType, List<MediaType> acceptMediaType, MultiValueMap<String, String> headersVariables) throws Exception {
 
         if(mediaType == null){
             return sendInterface(url, charSet, Clazz, t, HttpMethod.GET, uriVariables, null, null, headersVariables);
@@ -51,16 +69,21 @@ public class HTTPConnection {
         }
     }
 
-    private <T> T sendInterface(String url, String charSet, Class<? extends T> Clazz, T t, HttpMethod method, MultiValueMap<? extends String, ? extends String> uriVariables, MediaType mediaType, List<MediaType> acceptMediaType, MultiValueMap<String, String> headersVariables){
-
-        Map<String, Object> result = new HashMap<>();
+    private <T> T sendInterface(String url, String charSet, Class<? extends T> Clazz, T t, HttpMethod method, MultiValueMap<? extends String, ? extends String> uriVariables, MediaType mediaType, List<MediaType> acceptMediaType, MultiValueMap<String, String> headersVariables) throws Exception {
 
         try {
             UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(url);
+            if(method == HttpMethod.GET){
+                Iterator<String> it = (Iterator<String>) uriVariables.keySet().iterator();
+                while (it.hasNext()){
+                    String keyStr = it.next();
+                    url += keyStr;
+                }
+            }
+            log.debug(url);
             url = uriBuilder.build().encode().toString();
 
-            HttpHeaders headers = new HttpHeaders();
-            headers = createHeaders(url, charSet, headersVariables);
+            HttpHeaders headers = createHeaders(mediaType, acceptMediaType, headersVariables);
 
             HttpEntity<?> requestEntity = null;
             if(t != null){
@@ -76,22 +99,37 @@ public class HTTPConnection {
             switch (response.getStatusCode()){
                 case OK:
                     break;
-                case CREATED:
-                case ACCEPTED:
-                case NOT_FOUND:
-                case NO_CONTENT:
                 case BAD_REQUEST:
-                case CHECKPOINT:
-                    log.error("요청 실패");
-                    break;
+                case UNAUTHORIZED:
+                case NOT_FOUND:
+                case INTERNAL_SERVER_ERROR:
+                case SERVICE_UNAVAILABLE:
+//                    throw new SendInterfaceException("내부 통신 실패", "999");
+                    throw new Exception("Exception");
+                default:
+//                    throw new SendInterfaceException("내부 통신 알수 없는 오류", "999");
+                    throw new Exception("Exception");
             }
 
-//            T t =  <T> response.getBody();
-
+            return (T) response.getBody();
+        }catch(HttpClientErrorException e){
+            log.error("HttpClientErrorException", e);
+            throw new Exception("HttpClientErrorException", e);
+        }catch(HttpServerErrorException e){
+            log.error("HttpServerErrorException", e);
+            throw new Exception("HttpServerErrorException", e);
+        }catch(RestClientException e){
+            log.error("RestClientException", e);
+            throw new Exception("RestClientException", e);
+        }catch(NestedRuntimeException e){
+            log.error("NestedRuntimeException", e);
+            throw new Exception("NestedRuntimeException", e);
+        }catch(InvalidMediaTypeException e){
+            log.error("InvalidMediaTypeException", e);
+            throw new Exception("InvalidMediaTypeException", e);
         }catch (Exception e){
-            e.getMessage();
+            log.error("Exception", e);
+            throw new Exception("Exception", e);
         }
-
-        return t;
     }
 }
