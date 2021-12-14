31b61bc192c93315de3085b063cccd1ae2a1f511
하동균 <gkehdrbssla@gmail.com>
2021-12-14 16:43:20 +0900

    test 입

--- src/main/java/com/example/groupware/repository/test/testRepository.java
+++ src/main/java/com/example/groupware/repository/test/testRepository.java
@@ -6,24 +6,28 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.Map;
 
 @Repository
 public class testRepository {
 
+    @PersistenceContext
+    EntityManager entityManager;
 //    public SessionFactory sessionFactory;
+//    Session session = sessionFactory.getCurrentSession();
 //
-//    public Session session = sessionFactory.getCurrentSession();
-//
-//    public List<Map<String,Object>> selectName(BoardMasterVO t){
-//        StringBuffer str = new StringBuffer();
-//        str.append("SELECT BOARD_TITLE ");
-//        str.append("FROM BOARD_MASTER ");
-//
-//        Query query = (Query) session.createQuery(str.toString()).getParameter(t.getBoardTitle());
-//
-//        List<Map<String,Object>> result = query.list();
-//        return result;
-//    }
+    public List<Map<String,Object>> selectName(BoardMasterVO t){
+        StringBuffer str = new StringBuffer();
+        str.append("SELECT BOARD_TITLE ");
+        str.append("FROM BOARD_MASTER ");
+
+        Query query = (Query) entityManager.createQuery(str.toString()).getParameter(t.getBoardTitle());
+
+        List<Map<String,Object>> result = query.getResultList();
+        return result;
+    }
 }
