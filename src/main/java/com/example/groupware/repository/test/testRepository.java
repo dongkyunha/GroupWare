f05a0d984ce8548b8317798ae59a158744ad69b7
하동균 <gkehdrbssla@gmail.com>
2021-12-14 16:04:33 +0900

    ..

--- src/main/java/com/example/groupware/repository/test/testRepository.java
+++ src/main/java/com/example/groupware/repository/test/testRepository.java
@@ -1,26 +1,29 @@
 package com.example.groupware.repository.test;
 
 import com.example.groupware.entity.board.BoardMasterVO;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Map;
+
 @Repository
 public class testRepository {
 
-    public SessionFactory sessionFactory;
-
-    public Session session = sessionFactory.getCurrentSession();
-    
-    public BoardMasterVO selectName(BoardMasterVO t){
-        StringBuffer str = new StringBuffer();
-        str.append("SELECT BOARD_TITLE ");
-        str.append("FROM BOARD_MASTER ");
-
-        session.createQuery(str.toString()).getParameter(t.getBoardTitle());
-
-        return t;
-    }
-
-
+//    public SessionFactory sessionFactory;
+//
+//    public Session session = sessionFactory.getCurrentSession();
+//
+//    public List<Map<String,Object>> selectName(BoardMasterVO t){
+//        StringBuffer str = new StringBuffer();
+//        str.append("SELECT BOARD_TITLE ");
+//        str.append("FROM BOARD_MASTER ");
+//
+//        Query query = (Query) session.createQuery(str.toString()).getParameter(t.getBoardTitle());
+//
+//        List<Map<String,Object>> result = query.list();
+//        return result;
+//    }
 }
